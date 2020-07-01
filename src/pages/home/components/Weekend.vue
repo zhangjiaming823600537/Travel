d46d5a743144f4b465babeb4801e1732a624c56a
zhangjiaming <823600537@qq.com>
2020-07-02 00:14:30 +0800

    feat:weekend组件开发完成

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item" v-for="item of recommendList" :key="item.id ">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+          title: '北京温泉排行榜',
+          desc: '细数北京温泉，温暖你的冬天'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+          title: '北京温泉排行榜',
+          desc: '细数北京温泉，温暖你的冬天'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+          title: '北京温泉排行榜',
+          desc: '细数北京温泉，温暖你的冬天'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+          title: '北京温泉排行榜',
+          desc: '细数北京温泉，温暖你的冬天'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.title {
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+
+.item-img-wrapper {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 37.09%;
+
+  .item-img {
+    width: 100%;
+  }
+}
+
+.item-info {
+  padding: 0.1rem;
+
+  .item-title {
+    line-height: 0.54rem;
+    font-size: 0.32rem;
+    ellipsis();
+  }
+
+  .item-desc {
+    line-height: 0.4rem;
+    color: #ccc;
+    ellipsis();
+  }
+}
+</style>
